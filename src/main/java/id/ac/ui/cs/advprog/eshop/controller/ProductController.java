74e87901912462472bb35b4f456157b7f59d7c51
Cyrillo Praditya Soeharto <cyrillo.praditya41@ui.ac.id>
2026-02-06 20:38:37 +0700

    delete product

--- src/main/java/id/ac/ui/cs/advprog/eshop/controller/ProductController.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/controller/ProductController.java
@@ -50,6 +50,12 @@ public class ProductController {
         service.update(product);
         return "redirect:list";
     }
+
+    @GetMapping("/delete/{productId}")
+    public String deleteProduct(@PathVariable("productId") String productId) {
+        service.delete(productId);
+        return "redirect:../list";
+    }
 }
 
 
