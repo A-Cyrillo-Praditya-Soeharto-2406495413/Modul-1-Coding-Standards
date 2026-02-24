23f1129146a87c8d1a511b218ab0409869128c80
Cyrillo Praditya Soeharto <cyrillo.praditya41@ui.ac.id>
2026-02-24 13:58:06 +0700

    Dockerfile fix and some  unit test

--- src/test/java/id/ac/ui/cs/advprog/eshop/EshopApplicationTests.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/EshopApplicationTests.java
@@ -1,13 +1,13 @@
 package id.ac.ui.cs.advprog.eshop;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.*;
 
-@SpringBootTest
 class EshopApplicationTests {
 
     @Test
-    void contextLoads() {
+    void testMain() {
+        String[] args = {};
+        assertDoesNotThrow(() -> EshopApplication.main(args));
     }
-
 }
