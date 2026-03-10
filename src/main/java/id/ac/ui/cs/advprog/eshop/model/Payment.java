a9f75cd891d585a49347b0da40f0439654d1c9fd
Cyrillo Praditya Soeharto <cyrillo.praditya41@ui.ac.id>
2026-03-10 19:38:46 +0700

    [GREEN] Implement Payment model class

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
@@ -1,10 +1,14 @@
 package id.ac.ui.cs.advprog.eshop.model;
 
+import lombok.Builder;
+import lombok.Getter;
 import java.util.Map;
 
+@Builder
+@Getter
 public class Payment {
-    String id;
-    String method;
-    String status;
-    Map<String, String> paymentData;
+    private String id;
+    private String method;
+    private String status;
+    private Map<String, String> paymentData;
 }
