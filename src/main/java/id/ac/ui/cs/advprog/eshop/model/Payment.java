1bdf176e893723e4a48d1d28b4a7e74c6505ece0
Cyrillo Praditya Soeharto <cyrillo.praditya41@ui.ac.id>
2026-03-10 19:37:13 +0700

    [RED] Add Payment model skeleton

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
@@ -0,0 +1,10 @@
+package id.ac.ui.cs.advprog.eshop.model;
+
+import java.util.Map;
+
+public class Payment {
+    String id;
+    String method;
+    String status;
+    Map<String, String> paymentData;
+}
